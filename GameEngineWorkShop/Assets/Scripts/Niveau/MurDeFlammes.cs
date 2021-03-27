b5d734cc884c5be37bc54815b8f1bbd0284c1797
ManoeTM <72621868+ManoeTM@users.noreply.github.com>
2021-03-27 16:17:27 +0100

    Prefab Mur de Flammes

---
+++ GameEngineWorkShop/Assets/Scripts/Niveau/MurDeFlammes.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MurDeFlammes : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float vitesse = 3.0f;
+    [SerializeField] private int hauteur;
+    private bool arreteDeBouger = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, hauteur, transform.localScale.y);
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.right * vitesse * Time.fixedDeltaTime);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!arreteDeBouger)
+        {
+            Move();
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        Cuby player;
+        if (other.transform.parent.TryGetComponent(out player))
+        {
+            text.gameObject.SetActive(true);
+            Destroy(player.gameObject);
+            arreteDeBouger = true;
+        }
+        
+    }
+}
