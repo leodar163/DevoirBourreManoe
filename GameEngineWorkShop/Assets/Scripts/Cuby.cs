ca0ebea14e53c49ca8bcdda3c4575b4ac789c393
ManoeTM <72621868+ManoeTM@users.noreply.github.com>
2021-03-27 18:27:23 +0100

    Section Prefabs
    30 sections à tester

--- GameEngineWorkShop/Assets/Scripts/Cuby.cs
+++ GameEngineWorkShop/Assets/Scripts/Cuby.cs
@@ -10,7 +10,7 @@ public class Cuby : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,6 +21,42 @@ public class Cuby : MonoBehaviour
 
     private void AffecterControles()
     {
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            Dash();
+
+        }
+    }
+
+    private void Dash()
+    {
+        RaycastHit rayCast;
+        if (Physics.Raycast(transform.position, viseur.transform.up, out rayCast, Mathf.Infinity, LayerMask.GetMask("Surface")))
+        {
+            viseur.gameObject.SetActive(false);
+            Time.timeScale = 0.01f;
+            transform.up = rayCast.normal;
+            transform.DOMove(rayCast.point, 1.5f * Time.timeScale).OnComplete(() => {
+                Time.timeScale = 1;
+                viseur.gameObject.SetActive(true);
+
+            });
+
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        RaycastHit ray;
+        Gizmos.color = Color.red;
+        if (Physics.Raycast(transform.position, viseur.transform.up, out ray, Mathf.Infinity, LayerMask.GetMask("Surface")))
+        {
+            Gizmos.DrawLine(transform.position, ray.point);
+        }
+        else
+        {
+            Gizmos.DrawLine(transform.position, viseur.transform.up * 10000);
+        }
     }
 }
