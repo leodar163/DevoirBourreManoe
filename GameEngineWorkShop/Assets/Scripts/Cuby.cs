b2e81f91621380966264e118a31a431ea3e0c791
ManoeTM <72621868+ManoeTM@users.noreply.github.com>
2021-03-27 14:23:47 +0100

    Dash + Gizmo Debug

--- GameEngineWorkShop/Assets/Scripts/Cuby.cs
+++ GameEngineWorkShop/Assets/Scripts/Cuby.cs
@@ -23,7 +23,38 @@ public class Cuby : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
+            Dash();
+            
+        }
+    }
+
+    private void Dash()
+    {
+        RaycastHit rayCast;
+        if (Physics.Raycast(transform.position, viseur.transform.up, out rayCast, Mathf.Infinity, LayerMask.GetMask("Surface")))
+        {
             viseur.gameObject.SetActive(false);
+            Time.timeScale = 0.01f;
+            transform.DOMove(rayCast.transform.position, 1.5f * Time.timeScale).OnComplete(() => {
+                Time.timeScale = 1;
+                viseur.gameObject.SetActive(true);
+            });
+            
+            
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        RaycastHit ray;
+        Gizmos.color = Color.red;
+        if (Physics.Raycast(transform.position, viseur.transform.up, out ray, Mathf.Infinity, LayerMask.GetMask("Surface")))
+        {
+            Gizmos.DrawLine(transform.position, ray.point);
+        }
+        else
+        {
+            Gizmos.DrawLine(transform.position, viseur.transform.up * 10000);
         }
     }
 }
