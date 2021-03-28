813222699d23dfe7eedc46fbbd4f7a99a0df1a38
ManoeTM <72621868+ManoeTM@users.noreply.github.com>
2021-03-28 19:26:07 +0200

    Mises à jours des Prefabs Sections

---
+++ GameEngineWorkShop/Assets/Scripts/CompteurDePiece.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class CompteurDePiece : MonoBehaviour
+{
+    public int coins = 0;
+    [SerializeField] public TextMeshProUGUI text;
+
+    private void Start()
+    {
+    }
+
+    public void Incrementer()
+    {
+        coins++;
+        text.SetText(coins.ToString());
+    }
+}
